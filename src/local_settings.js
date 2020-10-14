18ac80347ef04cf288f444954cc260af00a5b7b5
Jora Nasos <horoshevvlad2000@mail.ru>
2020-10-14 17:29:44 +0700

    First_stage(version 1.3)

--- src/local_settings.js
+++ src/local_settings.js
@@ -1,4 +1,4 @@
 
 
-export let server_path = "http://192.168.0.3:8000"
-// export let server_path = "https://egida.herokuapp.com"
+// export let server_path = "http://192.168.0.3:8000"
+export let server_path = "https://egida.herokuapp.com"
