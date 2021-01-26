f5e96333037007c94da034892d4349cb2b2cb092
Jora Nasos <horoshevvlad2000@mail.ru>
2021-01-26 11:34:27 +0700

    First_stage(version 1.12) Fix Temperature

--- src/local_settings.js
+++ src/local_settings.js
@@ -3,4 +3,4 @@
 // export let server_path = "http://192.168.0.3:8000"
 // export let server_path = "https://egida.herokuapp.com"
 // export let server_path = "http://25.13.184.214:8000"
-export let server_path = "192.168.80.84:8000"
+export let server_path = "127.0.0.1:8000"
