29c6819a3d72991dbe97b3ba31ca622d7fd6205c
Jora Nasos <horoshevvlad2000@mail.ru>
2021-03-26 19:08:51 +0700

    Hot fix

--- src/local_settings.js
+++ src/local_settings.js
@@ -1,8 +1,8 @@
 
 
 // export let server_path = "http://192.168.1.7:8000"
-// export let server_path = "https://egida.herokuapp.com"
-export let server_path = "http://mtbou.nios.ru/back-end"
+export let server_path = "https://egida.herokuapp.com"
+// export let server_path = "http://mtbou.nios.ru/back-end"
 // export let server_path = "http://25.75.36.179:8000"
 // export let server_path = "http://127.0.0.1:8000"
 // export let server_path = "http://25.27.128.88:8000"
